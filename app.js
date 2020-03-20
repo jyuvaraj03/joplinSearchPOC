fe0026752b5c434e0e1177fb8441633e2040d1da
Yuvaraj Jayavel <jyuvaraj03@gmail.com>
2020-03-20 22:02:43 +0530

    Format timing comparison

--- app.js
+++ app.js
@@ -19,25 +19,34 @@ app.get('/search', function(req, res, next) {
             res.send(error);
             return;
         } else {
-            console.log("Connected successfully to SQLite");
+            console.log("Connected successfully to DB");
             res.sendStatus(200);
         }
     });
 
-    console.time('whole');  // start the timing for the whole search
-    
+    const searchQuery = req.query.searchTerm;
+    console.log(`Search Query: ${searchQuery}`);
+    console.time('pure_fts');    // start timing for pure FTS
+    // this is the sql query for FTS used in the current search engine in Joplin
+    const sql1 = `SELECT notes_fts.id, notes_fts.title AS normalized_title, offsets(notes_fts) AS offsets, notes.title, notes.body, notes.user_updated_time, notes.is_todo, notes.todo_completed, notes.parent_id FROM notes_fts LEFT JOIN notes ON notes_fts.id = notes.id WHERE notes_fts MATCH '${searchQuery}'`;
+    db.all(sql1, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        console.timeLog('pure_fts', `No. of results: ${rows.length}`);
+        console.timeEnd('pure_fts'); // stop timing for pure FTS
+    });
+
+    console.time('fuzzy');  // start the timing for fuzzy search using fusejs
     // the below sql query is a modification of the query used in the current search engine in Joplin
     // here ALL the notes are selected from the SQLite DB instead of only notes which match given term
     // and then passed to Fuse.JS for fuzzy searching
-    const sql = "SELECT notes_fts.id, notes_fts.title AS normalized_title, offsets(notes_fts) AS offsets, notes.title, notes.body, notes.user_updated_time, notes.is_todo, notes.todo_completed, notes.parent_id FROM notes_fts LEFT JOIN notes ON notes_fts.id = notes.id";
-    db.all(sql, [], (err, rows) => {
+    const sql2 = "SELECT notes_fts.id, notes_fts.title AS normalized_title, offsets(notes_fts) AS offsets, notes.title, notes.body, notes.user_updated_time, notes.is_todo, notes.todo_completed, notes.parent_id FROM notes_fts LEFT JOIN notes ON notes_fts.id = notes.id";
+    db.all(sql2, [], (err, rows) => {
         if (err) {
             throw err;
         } 
 
-        console.time('fuzzy');  // start the timing for fuzzy search using fusejs
-
-        let searchQuery = req.query.searchTerm;
         let options = {
             shouldSort: true,
             threshold: 0.3,
@@ -52,12 +61,9 @@ app.get('/search', function(req, res, next) {
         let fuse = new Fuse(rows, options); // "rows" is the result rows from SQL query
         let result = fuse.search(searchQuery ? searchQuery : '');
 
-        console.timeLog('fuzzy', result.length);
+        console.timeLog('fuzzy', `No. of results: ${result.length}`);
         console.timeEnd('fuzzy');   // stop the timing for fuzzy search using fusejs
-    
-        console.timeEnd('whole');   // stop the timing for the whole event
     });
-    
     db.close();
 });
 
